395eaa03726f72bece27b57dcd2537c93a2e2d5c
Frank Pak <fpak210@gmailc.om>
2017-04-21 23:44:46 -0700

    add anchorScroll

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,9 @@
-angular.module('userApp', ['appRoutes']);
+angular.module('userApp', [])
+  .controller('ScrollController', function($scope, $location, $anchorScroll) {
+      $scope.scrollTo = function(scrollLocation) {
+        $location.hash(scrollLocation);
+        $anchorScroll.yOffset = 20;
+        $anchorScroll();
+        $location.hash(old)
+      };
+    });
